560ece08372bfb83511e932aa3690e44d767b835
CBPAY-stats <pedogregoriano@gmail.com>
2025-07-10 11:22:28 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react( ),tailwindcss()],
-  base: '/explorer/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -15,13 +15,14 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 4173,
-    allowedHosts: ['4173-ihgpcs29gz8m4mc3fus1z-68c383e6.manusvm.computer', '4173-ig0l35mn6zkrjhgi9246h-4c198e94.manusvm.computer', 'localhost', '127.0.0.1']
+    allowedHosts: ['4173-imv9h581avvhhjw58wkf9-33be46f2.manusvm.computer', '4173-ihgpcs29gz8m4mc3fus1z-68c383e6.manusvm.computer', '4173-ig0l35mn6zkrjhgi9246h-4c198e94.manusvm.computer', 'localhost', '127.0.0.1']
   },
   preview: {
     port: 4173,
     host: '0.0.0.0',
     strictPort: true,
-    allowedHosts: ['4173-ihgpcs29gz8m4mc3fus1z-68c383e6.manusvm.computer', '4173-ig0l35mn6zkrjhgi9246h-4c198e94.manusvm.computer', 'localhost', '127.0.0.1']
+    allowedHosts: ['4173-imv9h581avvhhjw58wkf9-33be46f2.manusvm.computer', '4173-ihgpcs29gz8m4mc3fus1z-68c383e6.manusvm.computer', '4173-ig0l35mn6zkrjhgi9246h-4c198e94.manusvm.computer', 'localhost', '127.0.0.1']
   }
 })
+
 
